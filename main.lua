430155f1dd18cb4e964240b14a0ca29275088f6a
DoctorBubs <joshua.jman.goodrich@gmail.com>
2023-12-03 20:25:52 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,13 +3,13 @@ local text = io.input("input.txt")
 local input = io.read("*all")
 
 
+--We use lume fo easier functional programming.
 
-
---symbols is a hash table that will contain a true value for each symbol we need to look for.
+local lume = require "lume"
+--Symbols is a hash table that will contain a true value for each symbol we need to look for.
 local symbols = {}
 
---We use lume fo easier functional programming.
-local lume = require "lume"
+
 
 --This function will take any string except ".", and make sure that symbols[string] is set to true.
 local function symbol_add(s)
@@ -36,14 +36,17 @@ for line in string.gmatch(input,"%C+") do
 end
 
 
--- We keep an array for each tile in the map that contains a symbol or a reference to a table with a number
+--[[ We keep an array for each tile in the map that contains a symbol or a reference to a table with a number. 
+Each tile contains an x and y position that corresponds to a char in the string, with y y position being an abstraction of each new line.
+]]
 local all_symbol_tiles = {}
-
 local all_number_tiles = {}
 
+-- We use this to keep track of the curreny y position when we create a new tile, it goes up by 1 for each line in the string.
 local y_counter = 0
--- we loop through each line in the input.
+-- We loop through each line in the input.
 for line in string.gmatch(input,"%C+") do 
+  
   y_counter = y_counter + 1
   local leng = string.len(line)
   
@@ -61,18 +64,20 @@ for line in string.gmatch(input,"%C+") do
     end
     
   end
-  -- _next, we loop through the line to find all numbers
+  -- Next, we loop through the line to find all numbers.
   local num_starting_point = 1
   while true do
     local start_index,end_index= string.find(line,"%d+",num_starting_point)
-    --If we can not find any more numbers, we break the loop
+    --If we can not find any more numbers, we break the loop.
     if not start_index then
       break
     else
       --If we find a number- we track where the number starts and end in the string.
       local value = string.sub(line,start_index,end_index)
-      -- We store the value in a shared table.
+      -- We store the full number in a table.
       local shared_table = { value = value}
+      --[[ For each digit in the number, we create a new number with an x and y position that corresponds with the digits position in the string.
+      Each tile also contains a pointer to the shared table that holds the full number]]
       for x = start_index,end_index do
         local tile ={}
         tile.has_num = true
@@ -85,14 +90,16 @@ for line in string.gmatch(input,"%C+") do
     end
   end
 end
-
+-- This function returns true if the distance between two tiles equals 1.
 local function dist_1(a,b)
   return math.abs(a.x_pos - b.x_pos) <= 1 and math.abs(a.y_pos - b.y_pos) <= 1 
 
 end
 
-
-local function get_adjecent_shared_tiles(symbol_tile)
+--[[ This function takes an tile that contains a symbol,and creates an array.
+For each number tile that is one space away,the tile's shared value table is inserted into the array.
+The function then clears the array of duplicates]]
+local function get_adjacent_shared_tiles(symbol_tile)
   
   return lume.chain(all_number_tiles)
   :filter(function(num_tile) return dist_1(num_tile,symbol_tile)end)
@@ -102,19 +109,23 @@ local function get_adjecent_shared_tiles(symbol_tile)
   
 end
 
+-- This array will contain every shared value table that is next to a symbol.
+local adjacent_shared_values = {}
 
-local adjecent_shared_values = {}
-print(string.format("total symbol tiles = %d",#all_symbol_tiles))
+--We get the adjecent shared values for each symbol tile
 for _,sym_tile in ipairs(all_symbol_tiles) do
-  local map_arr = get_adjecent_shared_tiles(sym_tile)
+  local map_arr = get_adjacent_shared_tiles(sym_tile)
+  -- and then insert those in adjacent_shared_values
   for i, shared_value in ipairs(map_arr) do
-    table.insert(adjecent_shared_values,shared_value)
+    table.insert(adjacent_shared_values,shared_value)
   end
 end
-
-local result = lume.chain(adjecent_shared_values)
+--[[ Finally, we clear adjecent_shared_values of all duplicate shared value table, then extract the actual value from each table, and add up each value to gvet the solution.
+]]
+local result = lume.chain(adjacent_shared_values)
   :unique()
   :map(function(t) return t.value end)
   :reduce(function(a,b) return a + b end)
   :result()
+
 print(result)
